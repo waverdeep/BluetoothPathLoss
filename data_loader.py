7fe913dd3c1eee129985aee78d3650f6076caccd
waverDeep <dandyleader@naver.com>
2021-04-19 21:20:10 +0900

    dataloader 부분 구현 완료

---
+++ data_loader.py
@@ -0,0 +1,27 @@
+import torch
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+
+class PathLossDataset(Dataset):
+    def __init__(self, input_dir):
+        self.dataset = pd.read_csv(input_dir)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        line = self.dataset.loc[0]
+        line = line.tolist()
+        del line[0]
+        y_data = line[0]
+        del line[0]
+        x_data = line
+        x_data = torch.Tensor(x_data, dtype=torch.float)
+        y_data = torch.Tensor(y_data, dtype=torch.float)
+        return x_data, y_data
+
+def load_pathloss_dataset(input_dir, batch_size, shuffle, num_workers):
+    pathloss_dataset = PathLossDataset(input_dir=input_dir)
+    pathloss_dataloader = DataLoader(pathloss_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+    return pathloss_dataloader
+
