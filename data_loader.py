3caf9430b23ac1ab412d0eb1f76f638a28963ce8
waverDeep <dandyleader@naver.com>
2021-04-20 12:51:48 +0900

    idx 오류 수정

--- data_loader.py
+++ data_loader.py
@@ -2,6 +2,7 @@ import torch
 import pandas as pd
 from torch.utils.data import DataLoader, Dataset
 
+
 class PathLossDataset(Dataset):
     def __init__(self, input_dir):
         self.dataset = pd.read_csv(input_dir)
@@ -10,15 +11,16 @@ class PathLossDataset(Dataset):
         return len(self.dataset)
 
     def __getitem__(self, idx):
-        line = self.dataset.loc[0]
+        line = self.dataset.loc[idx]
         line = line.tolist()
         del line[0]
         y_data = line[0]
         del line[0]
         x_data = line
-        x_data = torch.Tensor(x_data, dtype=torch.float)
-        y_data = torch.Tensor(y_data, dtype=torch.float)
+        x_data = torch.tensor(x_data, dtype=torch.float)
+        y_data = torch.tensor(y_data, dtype=torch.float)
         return x_data, y_data
+
 
 def load_pathloss_dataset(input_dir, batch_size, shuffle, num_workers):
     pathloss_dataset = PathLossDataset(input_dir=input_dir)
