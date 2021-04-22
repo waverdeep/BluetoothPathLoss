dcc946997db54cbc06d87e2fb3f76e193fc84370
waverDeep <dandyleader@naver.com>
2021-04-22 15:47:43 +0900

    vanilla lstm 모델 추가

--- model.py
+++ model.py
@@ -21,8 +21,32 @@ class VanillaNetwork(nn.Module):
         return self.linear_relu_stack(x)
 
 
+class VanillaLSTMNetwork(nn.Module):
+    def __init__(self, input_size=2):
+        super(VanillaLSTMNetwork, self).__init__()
+        self.lstm01 = nn.LSTM(input_size=input_size, hidden_size=128, num_layers=4, batch_first=True)
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(128, 64),
+            nn.Dropout(0.3),
+            nn.PReLU(),
+            nn.Linear(64, 32),
+            nn.Dropout(0.3),
+            nn.PReLU(),
+            nn.Linear(32, 1)
+        )
+
+    def forward(self, x):
+        output, _ = self.lstm01(x)
+        return self.linear_relu_stack(output[:, -1, :])
+
 if __name__ == '__main__':
-    model = VanillaNetwork()
+
+    kind = 'RNN'
+
+    if kind == 'ANN':
+        model = VanillaNetwork()
+    elif kind == 'RNN':
+        model = VanillaLSTMNetwork()
     print("Model structure: ", model, "\n\n")
     for name, param in model.named_parameters():
         print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
