f6c635bbcf54a0f4ba58bb51c1cacd483e33ecd0
waverDeep <dandyleader@naver.com>
2021-04-23 15:46:03 +0900

    metrics 테스트에 frechet distance 추가

--- metrics.py
+++ metrics.py
@@ -1,14 +1,21 @@
 import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_error
+from frechetdist import frdist
 
-def MAPE(y_data, pred):
+
+def mean_absolute_percentage_error(y_data, pred):
     y_data = np.array(y_data)
     pred = np.array(pred)
-
     return np.mean(np.abs((y_data-pred)/y_data))*100
 
 
-def MPE(y_data, pred):
+def mean_percentage_error(y_data, pred):
     y_data = np.array(y_data)
     pred = np.array(pred)
-
     return np.mean((y_data-pred)/y_data)*100
+
+
+if __name__ == '__main__':
+    print('metrics test')
