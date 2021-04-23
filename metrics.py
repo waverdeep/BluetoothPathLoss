d8edcc035361c86c2c8e39e132a9f706abeb74a7
waverDeep <dandyleader@naver.com>
2021-04-23 21:17:59 +0900

    max/min error distance 함수 정의

--- metrics.py
+++ metrics.py
@@ -17,5 +17,37 @@ def mean_percentage_error(y_data, pred):
     return np.mean((y_data-pred)/y_data)*100
 
 
+def max_error_distance(y_data, pred):
+    y_data = np.array(y_data)
+    pred = np.array(pred)
+    substract = np.abs(y_data - pred)
+    return np.max(substract)
+
+
+def min_error_distance(y_data, pred):
+    y_data = np.array(y_data)
+    pred = np.array(pred)
+    substract = np.abs(y_data - pred)
+    return np.min(substract)
+
+
+def frechet_distance(rssi, y_data, pred):
+    data_length = len(rssi)
+    reshape_y_data = []
+    reshape_pred = []
+    for idx in range(data_length):
+        reshape_y_data.append([rssi[idx], y_data[idx]])
+        reshape_pred.append([rssi[idx], pred[idx]])
+
+    frechet_result = frdist(reshape_y_data, reshape_pred)
+    return frechet_result
+
+
+
 if __name__ == '__main__':
     print('metrics test')
+    # frechet distance
+    # rssi, distance 쌍을 집어넣어야 확인할 수 있음
+    P = [[1, 1], [2, 1]]
+    Q = [[2, 2], [2, 1]]
+    print('frechet distance : {}'.format(frdist(P, Q)))
