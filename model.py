178904769d725f0b2af7977d98310f18b9fc55ca
waverDeep <dandyleader@naver.com>
2021-04-29 19:47:35 +0900

    rnn 모델 구조 수정 ( 레이어 개수 감소 등 세부 파라미터 수정 )

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from torch.autograd import Variable
 
 
 class VanillaNetwork(nn.Module):
@@ -24,19 +25,25 @@ class VanillaNetwork(nn.Module):
 class VanillaLSTMNetwork(nn.Module):
     def __init__(self, input_size=2):
         super(VanillaLSTMNetwork, self).__init__()
-        self.lstm01 = nn.LSTM(input_size=input_size, hidden_size=128, num_layers=4, batch_first=True)
+        self.hidden_size = 128
+        self.num_layers = 2
+        self.lstm01 = nn.LSTM(input_size=input_size, hidden_size=self.hidden_size, num_layers=self.num_layers,
+                              batch_first=True)
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(128, 64),
+            nn.Linear(self.hidden_size, 64),
             nn.Dropout(0.3),
-            nn.PReLU(),
-            nn.Linear(64, 32),
-            nn.Dropout(0.3),
-            nn.PReLU(),
-            nn.Linear(32, 1)
+            nn.ReLU(),
+            nn.Linear(64, 1)
         )
 
     def forward(self, x):
-        output, _ = self.lstm01(x)
+        h_0 = Variable(torch.zeros(
+            self.num_layers, x.size(0), self.hidden_size)).cuda()
+
+        c_0 = Variable(torch.zeros(
+            self.num_layers, x.size(0), self.hidden_size)).cuda()
+        output, (h_out, _) = self.lstm01(x, (h_0, c_0))
+        output, _ = self.lstm01(x, (h_0, c_0))
         return self.linear_relu_stack(output[:, -1, :])
 
 
