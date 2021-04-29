341e77471e77d0f421e17e9bc824f02f1c94edde
waverDeep <dandyleader@naver.com>
2021-04-29 13:34:19 +0900

    rnn 관련 데이터셋이 변경되었기 때문에 그에 맞춰서 코드 수정 / sequence length를 마음대로 조절하지 못하는 단점이 있음

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,8 +1,15 @@
 import torch
 import pandas as pd
 from torch.utils.data import DataLoader, Dataset
+import glob
+import os
 from sklearn.preprocessing import RobustScaler
 from sklearn.preprocessing import StandardScaler
+
+
+def get_all_file_path(input_dir, file_extension):
+    temp = glob.glob(os.path.join(input_dir, '**', '*.{}'.format(file_extension)), recursive=True)
+    return temp
 
 
 class PathLossDataset(Dataset):
@@ -30,54 +37,32 @@ class PathLossTimeSeriesDataset(Dataset):
     # --> train관련 파일이 1개 혹은 2개 3개가 있을 수 있음
     # 정렬이 제대로 되어있다고 가정하고 코드를 짜야 함
     # 즉 전처리 과정에서 데이터가 꼭 잘 들어있어야 함. (추후에는 데이터셋에 TimeStamp를 추가해야할 것)
-    def __init__(self, input_dir, sequence_length): # 리스트로 받을 지 결정
+    def __init__(self, input_dir, scaler='None'): # 리스트로 받을 지 결정
         self.input_dir = input_dir
-        self.dataset = self.reshape_dataset(sequence_length)
+        self.filelist = get_all_file_path(input_dir, file_extension='csv')
         pass
 
     def __len__(self):
-        return len(self.dataset)
+        return len(self.filelist)
 
-    def __getitem__(self, item):
-        pass
-
-    def reshape_dataset(self, sequence_length):
-        dataset = []
-        divide_dataset = []
-        files = []
-        meter_status = -1
-        for file in self.input_dir:
-            raw_data = pd.read_csv(file, names=['mac', 'meter', 'rssi',
-                                                'tx_power', 'tx_height',
-                                                'rx_height', 'tx_antenna_gain',
-                                                'rx_antenna_gain', 'FSPL',
-                                                'environment'])
-            temp = []
-            for index, row in raw_data.iterrows():
-                if row['meter'] != meter_status:
-                    if len(temp) != 0:
-                        divide_dataset.append(temp)
-                        temp = []
-                        meter_status = row['meter']
-                        temp.append(row.tolist())
-
-                temp.append(row.tolist())
-
-        # ----- #
-        for div in divide_dataset:
-            div_len = len(div)
-            for j in range(0, (div_len-sequence_length)):
-                dataset.append(div[j:j+sequence_length])
-
-
-
-        return dataset
+    def __getitem__(self, idx):
+        file = self.filelist[idx]
+        file = pd.read_csv(file)
+        y_label = file[0][0]
+        y_label = y_label.to_numpy()
+        file = file.drop(file.columns[0], axis='columns')
+        x_label = file.to_numpy()
+        return torch.Tensor(x_label), torch.Tensor(y_label)
 
 
 def load_pathloss_dataset(input_dir, batch_size, shuffle, num_workers, type='ANN'):
     if type == 'ANN':
         pathloss_dataset = PathLossDataset(input_dir=input_dir, scaler='robust')
-        pathloss_dataloader = DataLoader(pathloss_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+        pathloss_dataloader = DataLoader(pathloss_dataset, batch_size=batch_size, shuffle=shuffle,
+                                         num_workers=num_workers)
         return pathloss_dataloader
     elif type == 'RNN':
-        pass
+        pathloss_dataset = PathLossTimeSeriesDataset(input_dir=input_dir, scaler='robust')
+        pathloss_dataloader = DataLoader(pathloss_dataset, batch_size=batch_size, shuffle=shuffle,
+                                         num_workers=num_workers)
+        return pathloss_dataloader
