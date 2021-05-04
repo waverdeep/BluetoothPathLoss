06bc2b3cafde1608985a521533296f9e78c3f988
waverDeep <dandyleader@naver.com>
2021-05-04 16:02:54 +0900

    rnn과 ffnn 둘다 사용 가능하도록 코드 변경 하지만 중복되는 코드가 많아 리팩토링을 해야 함

--- ann/playground_for_ann.py
+++ ann/playground_for_ann.py
@@ -35,11 +35,12 @@ def train(model_config, count, writer_name):
                                                                       model_type=model_config['model'],
                                                                       num_workers=model_config['num_workers'],
                                                                       batch_size=model_config['batch_size'],
-                                                                      shuffle=model_config['shuffle'])
+                                                                      shuffle=model_config['shuffle'],
+                                                                      input_size=model_config['input_size'])
     num_epochs = model_config['epoch']
     if model_config['model'] == 'FFNN':
         nn_model = model.VanillaNetwork(model_config['input_size']).cuda()
-        criterion = optimizer.set_criterion(model_config['criterion'])
+        criterion = optimizer.set_criterion(model_config['criterion']).cuda()
         optim = optimizer.set_optimizer(model_config['optimizer'],
                                         nn_model, model_config['learning_rate'])
         writer = set_tensorboard_writer('{}/testcase{}'.format(writer_name, str(count).zfill(3)))
@@ -68,7 +69,7 @@ def train(model_config, count, writer_name):
             torch.save({epoch: epoch,
                         'model': nn_model,
                         'model_state_dict': nn_model.state_dict()},
-                       "../checkpoints/testcase{}_epoch_{}.pt".format(str(count).zfill(3), epoch))
+                       "../checkpoints_v1/testcase{}_epoch_{}.pt".format(str(count).zfill(3), epoch))
 
             if (epoch + 1) % 10 == 0:
                 with torch.no_grad():
@@ -109,16 +110,83 @@ def train(model_config, count, writer_name):
                     print("MSE Score : {}".format(test_mse_score))  # 평균제곱 오차가음 낮을수록 좋음
                     print("R2 Score : {}".format(test_r2_score))
                     print("MAE Score : {}".format(test_mae_score))
+
+    elif model_config['model'] == 'RNN':
+        nn_model = model.VanillaRecurrentNetwork(model_config['input_size']).cuda()
+        criterion = optimizer.set_criterion(model_config['criterion']).cuda()
+        optim = optimizer.set_optimizer(model_config['optimizer'],
+                                        nn_model, model_config['learning_rate'])
+        writer = set_tensorboard_writer('{}/testcase{}'.format(writer_name, str(count).zfill(3)))
+
+        for epoch in range(num_epochs):
+            for i, data in enumerate(train_dataloader):
+                y_pred = nn_model(data[:][0].cuda()).reshape(-1)
+                y_data = data[:][1].cuda()
+                loss = criterion(y_pred, y_data)
+                # print('x_data: ', data[:][0])
+                # print('x_data shape: ', data[:][0].shape)
+                # print('y_data: ', y_data)
+                # print('y_pred: ', y_pred)
+                loss.backward()
+                optim.step()
+
+                # ...학습 중 손실(running loss)을 기록하고
+                writer.add_scalar('mseloss training loss',
+                                  loss / 1000,
+                                  epoch * len(train_dataloader) + i)
+
+                if (epoch + 1) % 5 == 0:
+                    print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs, loss.item()))
+
+            if (epoch + 1) % 10 == 0:
+                with torch.no_grad():
+                    total_label = []
+                    total_pred = []
+                    for i, data in enumerate(test_dataloader):
+                        y_pred = nn_model(data[:][0].cuda()).reshape(-1)
+                        y_pred = y_pred.cpu()
+                        # rssi = x_data[:, 0]
+                        # rssi = rssi.cpu().numpy()
+                        y_data = data[:][1].numpy()
+                        y_pred = y_pred.numpy()
+                        # print('y_data : ', y_data)
+                        # print('y_pred : ', y_pred)
+
+                        total_label += y_data.tolist()
+                        total_pred += y_pred.tolist()
+
+                    test_mse_score = mean_squared_error(total_label, total_pred)
+                    test_r2_score = r2_score(total_label, total_pred)
+                    test_mae_score = mean_absolute_error(total_label, total_pred)
+                    test_rmse_score = np.sqrt(test_mse_score)
+
+                    writer.add_scalar('MSE Score',
+                                      test_mse_score,
+                                      epoch)
+                    writer.add_scalar('R2 Score',
+                                      test_r2_score,
+                                      epoch)
+                    writer.add_scalar('MAE Score',
+                                      test_mae_score,
+                                      epoch)
+                    writer.add_scalar('RMSE Score',
+                                      test_rmse_score,
+                                      epoch)
+
+                    print("MSE Score : {}".format(test_mse_score))  # 평균제곱 오차가음 낮을수록 좋음
+                    print("R2 Score : {}".format(test_r2_score))
+                    print("MAE Score : {}".format(test_mae_score))
+
     close_tensorboard_writer(writer)
 
 
 if __name__ == '__main__':
     model_config = {
         # model config
-        'model': 'FFNN', 'criterion': 'MSELoss',
-        'optimizer': 'AdaDelta', 'learning_rate': 1,
-        'cuda': True, 'batch_size': 128,
-        'epoch': 800, 'input_size': 8,
+        'model': 'RNN', 'criterion': 'MSELoss',
+        'optimizer': 'AdaDelta', 'learning_rate': 0.001,
+        'cuda': True, 'batch_size': 256,
+        'epoch': 800, 'input_size': 10,
         # dataset config
         'input_dir': '../dataset/v1_scaled',
         'shuffle': True, 'num_workers': 8,
