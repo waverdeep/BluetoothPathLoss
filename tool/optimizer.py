6a76d529fac6cf8d6d15c0f1a0ef9b888a81d4ff
waverDeep <dandyleader@naver.com>
2021-05-04 14:23:31 +0900

    오타 수정

--- tool/optimizer.py
+++ tool/optimizer.py
@@ -3,15 +3,15 @@ import torch
 
 def set_optimizer(name, model, learning_rate):
     if name == 'AdaDelta':
-        return torch.optim.Adadelta(model.parameter(), learning_rate)
+        return torch.optim.Adadelta(model.parameters(), learning_rate)
     elif name == 'AdaGrad':
-        return torch.optim.AdaGrad(model.parameter(), learning_rate)
+        return torch.optim.AdaGrad(model.parameters(), learning_rate)
     elif name == 'Adam':
-        return torch.optim.Adam(model.parameter(), learning_rate)
+        return torch.optim.Adam(model.parameters(), learning_rate)
     elif name == 'AdamW':
-        return torch.optim.AdamW(model.parameter(), learning_rate)
+        return torch.optim.AdamW(model.parameters(), learning_rate)
     elif name == 'SGD':
-        return torch.optim.SGD(model.parameter(), learning_rate)
+        return torch.optim.SGD(model.parameters(), learning_rate)
 
 
 def set_criterion(name):
