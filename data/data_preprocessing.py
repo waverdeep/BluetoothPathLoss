621ecdec516ac2ccfa860ddd5b62c84a8ac3685f
waverDeep <dandyleader@naver.com>
2021-05-04 12:02:40 +0900

    학습시 필요한 데이터의 형태를 미리 가공하는 형태 (폴더 자체로 묶어서 진행하기 때문에 이 코드도 마찬가지로 environment를 개별 파일로 반영할 수 없다,)

--- data/data_preprocessing.py
+++ data/data_preprocessing.py
@@ -1,10 +1,55 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import tool.path_loss as path_loss
+import glob
+import os
+import numpy as np
+from sklearn.externals import joblib
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import StandardScaler
+
+
+def get_all_file_path(input_dir, file_extension):
+    temp = glob.glob(os.path.join(input_dir, '**', '*.{}'.format(file_extension)), recursive=True)
+    return temp
+
+
+def create_directory(dir_path):
+    try:
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+    except OSError:
+        print('Error : Creating directory: '+dir_path)
+
+
+def set_scaler(name):
+    if name == 'MinMaxScaler':
+        return MinMaxScaler()
+    elif name == 'RobustScaler':
+        return RobustScaler()
+    elif name == 'StandardScaler':
+        return StandardScaler()
+
+
+def fit_selected_scaler(addition_dataset, scaler='None'):
+    tied_scaler = None
+    if scaler != 'None' and scaler == 'MinMaxScaler':
+        temp = []
+        tied_scaler = set_scaler(scaler)
+        for pack in addition_dataset:
+            for line in pack:
+                temp.append(line)
+        temp = np.array(temp)
+        temp = np.delete(temp, 0, axis=1)
+        tied_scaler.fit(temp)
+    save_scaler_name = './{}_saved.pkl'.format(scaler)
+    joblib.dump(tied_scaler, save_scaler_name)
+    return tied_scaler
 
 
 def get_train_test_valid():
-    data = pd.read_csv('dataset/2021_04_15/rssi.csv')
+    data = pd.read_csv('../dataset/v1_timeline')
     data2 = pd.read_csv('dataset/2021_04_15/rssi_s.csv')
 
     result = pd.concat([data, data2])
@@ -28,5 +73,59 @@ def get_train_test_valid():
     valid.to_csv('dataset/2021_04_15/pathloss_v1_valid.csv', header=None, index=None)
 
 
+def get_addition_dataset(input_dir, config):
+    file_list = get_all_file_path(input_dir=input_dir, file_extension='csv')
+    target_dataset = []
+    addition_dataset = []
+
+    for file in file_list:
+        temp = pd.read_csv(file)
+        temp = temp[temp['mac'] == config['device_id']]
+        temp = temp.drop(['mac', 'type'], axis=1)
+        target_dataset.append(temp)
+
+    for item in target_dataset:
+        temp = []
+        for idx, line in item.iterrows():
+            data = line.tolist()
+            data.append(config.get('tx_power'))
+            data.append(config.get('tx_height'))
+            data.append(config.get('rx_height'))
+            data.append(config.get('tx_antenna_gain'))
+            data.append(config.get('rx_antenna_gain'))
+            data.append(config.get('environment'))
+            if config['use_fspl']:
+                data.append(path_loss.get_distance_with_rssi_fspl(data[1]))
+            temp.append(data)
+        addition_dataset.append(temp)
+
+    if config['scaler'] == 'None':
+        pass
+    if config['scaler'] == 'MinMaxScaler':
+        tied_scaler = fit_selected_scaler(addition_dataset=addition_dataset, scaler=config['scaler'])
+        for idx, item in enumerate(addition_dataset):
+            temp = np.array(item)
+            y_label = temp[:, 0]
+            y_label = np.expand_dims(y_label, axis=1)
+            print(y_label)
+            x_data = np.delete(temp, 0, axis=1)
+            x_data = tied_scaler.transform(x_data)
+            total = np.concatenate((y_label, x_data), axis=1)
+            addition_dataset[idx] = total
+
+    for idx, item in enumerate(addition_dataset):
+        temp = pd.DataFrame(item)
+        create_directory(config['save_dir'])
+        temp.to_csv('{}/dataset_{}.csv'.format(config['save_dir'], idx) , header=None, index=None)
+
+
 if __name__ == '__main__':
-    pass
+    input_dir = '../dataset/v1_timeline'
+    device_id = 'f8:8a:5e:2d:80:f4'
+    config = {
+        'tx_power': 5, 'tx_height': 2, 'rx_height': 0.01, 'tx_antenna_gain': -1.47,
+        'rx_antenna_gain': -1, 'environment': 1, 'device_id': 'f8:8a:5e:2d:80:f4',
+        'use_fspl': True, 'save_dir': '../dataset/v1_scaled/', 'scaler': 'MinMaxScaler'
+    }
+    get_addition_dataset(input_dir=input_dir, config=config)
+
