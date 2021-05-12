2f95ef00f6dd67f158ccec59670a57b22391d4ec
waverDeep <dandyleader@naver.com>
2021-05-12 22:12:52 +0900

    데이터 로더 형태가 잘 갖춰저 있는지 확인

--- data/data_loader.py
+++ data/data_loader.py
@@ -17,6 +17,7 @@ def data_split(dataset, test_size=0.3):
     test_data, valid_data = train_test_split(test_data, test_size=test_size, shuffle=True, random_state=42)
     return train_data, test_data, valid_data
 
+
 class PathLossWithDetailDataset(Dataset):
     def __init__(self, input_data,  model_type='FFNN'):
         self.model_type = model_type
@@ -97,83 +98,10 @@ def load_path_loss_with_detail_dataset(input_dir, model_type='RNN',
 
 
 if __name__ == '__main__':
-    load_path_loss_with_detail_dataset('../dataset/v1_scaled', model_type='RNN')
+    train_dataloader, test_dataloader, validation_dataloader = load_path_loss_with_detail_dataset('../dataset/v1_scaled', model_type='FFNN')
+    print('train_dataloader : ', len(train_dataloader))
+    for data in train_dataloader:
+        print(data[:][0].shape)
+        print(data[:][1].shape)
+        break
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##-------------------old version code-----------------------##
-class PathLossDataset(Dataset):
-    def __init__(self, input_dir, scaler='None'):
-        self.dataset = pd.read_csv(input_dir, header=None)
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, idx):
-        line = self.dataset.loc[idx]
-        line = line.tolist()
-        del line[0]
-        y_data = line[0]
-        del line[0]
-
-        x_data = line
-        x_data = torch.tensor(x_data, dtype=torch.float)
-        y_data = torch.tensor(y_data, dtype=torch.float)
-        return x_data, y_data
-
-
-class PathLossTimeSeriesDataset(Dataset):
-    # dataset
-    # --> train관련 파일이 1개 혹은 2개 3개가 있을 수 있음
-    # 정렬이 제대로 되어있다고 가정하고 코드를 짜야 함
-    # 즉 전처리 과정에서 데이터가 꼭 잘 들어있어야 함. (추후에는 데이터셋에 TimeStamp를 추가해야할 것)
-    def __init__(self, dataset, scaler='None'): # 리스트로 받을 지 결정
-        self.dataset = dataset
-        pass
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, idx):
-        file = self.dataset[idx]
-        file = pd.read_csv(file, header=None)
-        y_label = file[0][0]
-        # y_label = y_label.to_numpy()
-        file = file.drop(file.columns[0], axis='columns')
-        x_label = file.to_numpy()
-        return torch.tensor(x_label, dtype=torch.float), torch.tensor(y_label, dtype=torch.float)
-
-
-def load_pathloss_dataset(input_dir, batch_size, shuffle, num_workers, type='ANN'):
-    if type == 'ANN':
-        pathloss_dataset = PathLossDataset(input_dir=input_dir, scaler='robust')
-        pathloss_dataloader = DataLoader(pathloss_dataset, batch_size=batch_size, shuffle=shuffle,
-                                         num_workers=num_workers)
-        return pathloss_dataloader
-    elif type == 'RNN':
-        pathloss_dataset = PathLossTimeSeriesDataset(dataset=input_dir, scaler='robust')
-        pathloss_dataloader = DataLoader(pathloss_dataset, batch_size=batch_size, shuffle=shuffle,
-                                         num_workers=num_workers)
-        return pathloss_dataloader
