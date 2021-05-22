ed58078834b0e95bb23b89f4fbc1a7d3604c26fc
waverDeep <dandyleader@naver.com>
2021-05-22 23:26:36 +0900

    파일 입출력 관련 코드 추가

---
+++ tool/file_io.py
@@ -0,0 +1,7 @@
+import glob
+import os
+
+
+def get_all_file_path(input_dir, file_extension):
+    temp = glob.glob(os.path.join(input_dir, '**', '*.{}'.format(file_extension)), recursive=True)
+    return temp
