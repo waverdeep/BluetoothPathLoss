432070fea89090c7adc6e31bad0a03b524d471d8
waverDeep <dandyleader@naver.com>
2021-06-05 20:35:21 +0900

    불필요한 코드 제거

--- model_config.py
+++ model_config.py
@@ -16,12 +16,5 @@ def set_activation(name):
 
 
 def set_recurrent_layer(name, input_size, hidden_size, num_layers, batch_first=True, bidirectional=False):
-    if name == 'RNN':
-        return nn.RNN(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=batch_first,
-                      bidirectional=bidirectional)
-    elif name == 'LSTM':
-        return nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=batch_first,
-                       bidirectional=bidirectional)
-    elif name == 'GRU':
-        return nn.GRU(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=batch_first,
-                      bidirectional=bidirectional)
+    return nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=batch_first,
+                   bidirectional=bidirectional)
