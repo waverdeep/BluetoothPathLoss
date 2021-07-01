2c2fe5259d003e2796b819e5f0d6983916e16326
waverDeep <dandyleader@naver.com>
2021-07-01 17:18:13 +0900

    모델 업데이트에 의한 수정 사항  - DNN 모델 수정  - RNN 모델 수정  - CRNN 모델 수정

--- model/model.py
+++ model/model.py
@@ -5,59 +5,122 @@ import model_config
 
 
 class VanillaNetwork(nn.Module):
-    def __init__(self, input_size=7, activation='PReLU'):
+    def __init__(self, input_size=7, activation='PReLU', linear=3):
         super(VanillaNetwork, self).__init__()
         self.activation = activation
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(input_size, 64),
-            model_config.set_activation(self.activation),
-            nn.Linear(64, 64),
-            nn.Dropout(0.3),
-            model_config.set_activation(self.activation),
-            nn.Linear(64, 1)
-        )
+
+        if linear == 3:
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(input_size, 64),
+                model_config.set_activation(self.activation),
+                nn.Linear(64, 64),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(64, 1)
+            )
+        elif linear == 4:
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(input_size, 32),
+                model_config.set_activation(self.activation),
+                nn.Linear(32, 64),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(64, 32),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(32, 1)
+            )
+        elif linear == 5:
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(input_size, 32),
+                model_config.set_activation(self.activation),
+                nn.Linear(32, 64),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(64, 64),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(64, 32),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(32, 1)
+            )
 
     def forward(self, x):
         return self.linear_relu_stack(x)
 
 
 class VanillaRecurrentNetwork(nn.Module):
-    def __init__(self, input_size=8, recurrent_model='LSTM', activation='PReLU', bidirectional=False):
+    def __init__(self, input_size=7, recurrent_model='LSTM', activation='PReLU', bidirectional=False,
+                 recurrent_hidden_size=64, recurrent_num_layers=1, linear=3, cuda=False):
         super(VanillaRecurrentNetwork, self).__init__()
         self.activation = activation
-        self.hidden_size = 64
-        self.num_layers = 1
-        self.lstm01 = model_config.set_recurrent_layer(name=recurrent_model, input_size=input_size,
-                                                       hidden_size=self.hidden_size, num_layers=self.num_layers,
-                                                       batch_first=True, bidirectional=bidirectional)
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(self.hidden_size, 64),
-            nn.Dropout(0.3),
-            model_config.set_activation(self.activation),
-            nn.Linear(64, 32),
-            nn.Dropout(0.3),
-            model_config.set_activation(self.activation),
-            nn.Linear(32, 1)
-        )
+        self.hidden_size = recurrent_hidden_size
+        self.num_layers = recurrent_num_layers
+        self.cuda = cuda
+        self.recurrent01 = model_config.set_recurrent_layer(name=recurrent_model, input_size=input_size,
+                                                            hidden_size=self.hidden_size, num_layers=self.num_layers,
+                                                            batch_first=True, bidirectional=bidirectional)
+        if linear == 3:
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(self.hidden_size, 64),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(64, 64),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(64, 1)
+            )
+        elif linear == 4:
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(self.hidden_size, 32),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(32, 64),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(64, 32),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(32, 1)
+            )
+        elif linear == 5:
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(self.hidden_size, 32),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(32, 64),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(64, 64),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(64, 32),
+                nn.Dropout(0.3),
+                model_config.set_activation(self.activation),
+                nn.Linear(32, 1)
+            )
 
     def forward(self, x):
-        h_0 = Variable(torch.zeros(
-            self.num_layers, x.size(0), self.hidden_size)).cuda()
-        c_0 = Variable(torch.zeros(
-            self.num_layers, x.size(0), self.hidden_size)).cuda()
-        output, (h_out, _) = self.lstm01(x, (h_0, c_0))
-        output, _ = self.lstm01(x, (h_0, c_0))
+        if self.cuda:
+            h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).cuda()
+            c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).cuda()
+        else:
+            h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+            c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+        output, (h_out, _) = self.recurrent01(x, (h_0, c_0))
         return self.linear_relu_stack(output[:, -1, :])
 
 
 class VanillaCRNNNetwork(nn.Module):
-    def __init__(self, input_size=8, recurrent_model='LSTM', activation='PReLU', bidirectional=False):
+    def __init__(self, input_size=7, recurrent_model='LSTM', activation='PReLU', bidirectional=False,
+                 recurrent_num_layeres=1, recurrent_hidden_size=256):
         super(VanillaCRNNNetwork, self).__init__()
         # convolution
-        self.num_layers = 1
-        self.hidden_size = 256
-        self.conv1d_layer = nn.Conv1d(8, 8, 3)
+        self.num_layers = recurrent_hidden_size
+        self.hidden_size = recurrent_hidden_size
         self.activation = activation
+        self.conv1d_layer = nn.Conv1d(input_size, input_size, 3)
         self.lstm_layer = model_config.set_recurrent_layer(name=recurrent_model, input_size=input_size,
                                                            hidden_size=self.hidden_size, num_layers=self.num_layers,
                                                            batch_first=True, bidirectional=bidirectional)
@@ -103,10 +166,10 @@ def model_load(model_configure):
 
 
 if __name__ == '__main__':
-    kind = 'DNN'
+    kind = 'CRNN'
     if kind == 'DNN':
         model = VanillaNetwork().cuda()
-    elif kind == 'LSTM':
+    elif kind == 'RNN':
         model = VanillaRecurrentNetwork()
     elif kind == 'CRNN':
         model = VanillaCRNNNetwork().cuda()
@@ -120,9 +183,11 @@ if __name__ == '__main__':
         x_data = torch.empty(7,).cuda()
         pred = model(x_data)
         print("pred : ", pred.shape)
-    elif kind == 'LSTM':
-        pass
+    elif kind == 'RNN':
+        x_data = torch.empty(1, 7, 7)
+        pred = model(x_data)
+        print('pred : ', pred.shape)
     elif kind == 'CRNN':
-        x_data = torch.empty(1, 8, 15).cuda()
+        x_data = torch.empty(1, 7, 15).cuda()
         pred = model(x_data)
         print('pred : ', pred.shape)
