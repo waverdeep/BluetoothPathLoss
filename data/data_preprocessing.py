dc7483ed89629cabf3b8b171c5a8d13648ffd09e
waverDeep <dandyleader@naver.com>
2021-07-01 19:37:52 +0900

    모델 업데이트에 의한 수정 사항  - device_id 가 있을때, 없을때를 파악하여 optional 하게 조절할 수 있도록 변경  - 안쓰는 코드 정리

--- data/data_preprocessing.py
+++ data/data_preprocessing.py
@@ -5,6 +5,59 @@ import numpy as np
 import tool.file_io as file_io
 
 
+def get_addition_dataset(config):
+    file_list = file_io.get_all_file_path(input_dir=config['input_dir'], file_extension='csv')
+    file_list.sort()
+    print(file_list)
+    target_dataset = []
+    addition_dataset = []
+
+    for file in file_list:
+        temp = pd.read_csv(file)
+        if config['device_id'] != '':
+            temp = temp[temp['mac'] == config['device_id']]
+        temp = temp.drop(['mac', 'type'], axis=1) # 불필요한 데이터 제거
+        target_dataset.append(temp)
+
+    for item in target_dataset:
+        temp = []
+        for idx, line in item.iterrows():
+            data = line.tolist()
+            data.append(config.get('tx_power'))
+            data.append(config.get('rx_height'))
+            data.append(config.get('tx_antenna_gain'))
+            data.append(config.get('rx_antenna_gain'))
+            data.append(config.get('covered'))
+            if config['use_fspl']:
+                data.append(path_loss.get_distance_with_rssi_fspl(data[1]))
+            if config['inference']:
+                del data[0]
+            temp.append(data)
+        addition_dataset.append(temp)
+
+    for idx, item in enumerate(addition_dataset):
+        temp = pd.DataFrame(item)
+        file_io.create_directory(config['save_dir'])
+        temp.to_csv('{}/dataset_{}_mac_{}.csv'.format(config['save_dir'], idx, config['device_id']), header=None, index=None)
+
+
+if __name__ == '__main__':
+    config_set = {
+        'input_dir': '../dataset/v4_test/dk_5_5',
+        'save_dir': '../dataset/v4_test/dk_convert_5_5/',
+        'tx_power': 8,
+        'rx_height': 2.0,
+        'tx_antenna_gain': -1,
+        'rx_antenna_gain': -1,
+        'covered': 1,
+        'device_id': 'f8:8a:5e:2d:80:f4', # optional
+        'use_fspl': True,
+        'inference': False
+    }
+    get_addition_dataset(config=config_set)
+
+
+#### not used #############################################################################################
 def get_train_test_valid():
     data = pd.read_csv('../dataset/v1_timeline')
     data2 = pd.read_csv('dataset/2021_04_15/rssi_s.csv')
@@ -28,54 +81,4 @@ def get_train_test_valid():
     train.to_csv('dataset/2021_04_15/pathloss_v1_train.csv', header=None, index=None)
     test.to_csv('dataset/2021_04_15/pathloss_v1_test.csv', header=None, index=None)
     valid.to_csv('dataset/2021_04_15/pathloss_v1_valid.csv', header=None, index=None)
-
-
-def get_addition_dataset(config):
-    file_list = file_io.get_all_file_path(input_dir=config['input_dir'], file_extension='csv')
-    file_list.sort()
-    print(file_list)
-    target_dataset = []
-    addition_dataset = []
-
-    for file in file_list:
-        temp = pd.read_csv(file)
-        # temp = temp[temp['mac'] == config['device_id']]
-        temp = temp.drop([ 'mac', 'type'], axis=1)
-        target_dataset.append(temp)
-
-    for item in target_dataset:
-        temp = []
-        for idx, line in item.iterrows():
-            data = line.tolist()
-            data.append(config.get('tx_power'))
-            data.append(config.get('tx_height'))
-            data.append(config.get('rx_height'))
-            data.append(config.get('tx_antenna_gain'))
-            data.append(config.get('rx_antenna_gain'))
-            data.append(config.get('environment'))
-            if config['use_fspl']:
-                data.append(path_loss.get_distance_with_rssi_fspl(data[1]))
-            del data[0] # inference
-            temp.append(data)
-        addition_dataset.append(temp)
-
-    for idx, item in enumerate(addition_dataset):
-        temp = pd.DataFrame(item)
-        file_io.create_directory(config['save_dir'])
-        temp.to_csv('{}/dataset_{}.csv'.format(config['save_dir'], idx) , header=None, index=None)
-
-
-if __name__ == '__main__':
-    config = {
-        'input_dir': '../dataset/v4_test/dk_5_5',
-        'save_dir': '../dataset/v4_test/dk_convert_5_5/',
-        'tx_power': 8,
-        'rx_height': 2.0,
-        'tx_antenna_gain': -1,
-        'rx_antenna_gain': -1,
-        'covered': 1,
-        'device_id': 'f8:8a:5e:2d:80:f4', # optional
-        'use_fspl': True,
-    }
-    get_addition_dataset(config=config)
 
