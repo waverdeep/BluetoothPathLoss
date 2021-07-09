75b5749d26b6294abede9cabb1b84cb866eb3d63
waverDeep <dandyleader@naver.com>
2021-07-09 15:04:56 +0900

    파아미터 지정 및 state를 설정하는 함수들을 model config 파이썬 파일로 이동

--- model/model_config.py
+++ model/model_config.py
@@ -1,4 +1,5 @@
 import torch
+from torch.autograd import Variable
 import torch.nn as nn
 
 
@@ -13,6 +14,34 @@ def set_activation(name):
         return nn.ELU()
     elif name == 'Tanh':
         return nn.Tanh()
+
+
+def n_gram(data_list, n=2):
+    result = []
+    for i in range(len(data_list) - (n - 1)):
+        result.append(data_list[i:i+n])
+    return result
+
+
+# param type : [11, 32, 64, 32, 1]
+def build_linear_layer(layer, linear_layers, activation, dropout_rate):
+    grapped_linear = n_gram(linear_layers)
+    print(grapped_linear)
+    for idx, line in enumerate(grapped_linear):
+        layer.add_module("linear_{}".format(idx), nn.Linear(line[0], line[1]))
+        if line[1] != 1:
+            layer.add_module("activation_{}".format(idx), set_activation(activation))
+            layer.add_module("dropout_{}".format(idx), nn.Dropout(dropout_rate))
+    return layer
+
+
+def set_state_h_c(num_layer, hidden_size, size, cuda):
+    h_0 = Variable(torch.zeros(num_layer, size, hidden_size))
+    c_0 = Variable(torch.zeros(num_layer, size, hidden_size))
+    if cuda:
+        h_0 = h_0.cuda()
+        c_0 = c_0.cuda()
+    return (h_0, c_0)
 
 
 def set_recurrent_layer(name, input_size, hidden_size, num_layers, batch_first=True, bidirectional=False):
