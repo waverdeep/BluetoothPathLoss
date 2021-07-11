5e51d9257ae2f019958a3c2889fb5c7f39799b40
waverDeep <dandyleader@naver.com>
2021-07-11 16:51:48 +0900

    현재 버전과 다음 버전의 config 차이를 반영하기 위한 코드 작성

--- playground.py
+++ playground.py
@@ -28,24 +28,37 @@ def sub_train(model_config, nn_model, dataloader, device, writer, epoch, optimiz
     for i, data in enumerate(dataloader):
         x_data = None
         y_data = None
-        if model_config['model'] == 'DNN':
-            x_data, y_data = data
-        elif model_config['model'] == 'RNN':
-            x_data = data[:][0]
-            y_data = data[:][1]
-        elif model_config['model'] == 'CRNN':
-            x_data = data[:][0]
-            x_data = x_data.transpose(1, 2)
-            y_data = data[:][1]
+        if 'model' in model_config:
+            if model_config['model'] == 'DNN':
+                x_data, y_data = data
+            elif model_config['model'] == 'RNN':
+                x_data = data[:][0]
+                y_data = data[:][1]
+            elif model_config['model'] == 'CRNN':
+                x_data = data[:][0]
+                x_data = x_data.transpose(1, 2)
+                y_data = data[:][1]
+        else:
+            if model_config['model_type'] == 'Custom_DNN':
+                x_data, y_data = data
+            elif model_config['model_type'] == 'Custom_RNN':
+                x_data = data[:][0]
+                y_data = data[:][1]
+            elif model_config['model_type'] == 'Custom_CRNN':
+                x_data = data[:][0]
+                x_data = x_data.transpose(1, 2)
+                y_data = data[:][1]
         if device:
             x_data = x_data.cuda()
             y_data = y_data.cuda()
         y_pred = nn_model(x_data).reshape(-1)
         optimizer.zero_grad()
         loss = criterion(y_pred, y_data)
+        writer.add_scalar('Loss/SubTrain MSELoss', loss / 1000, epoch * len(dataloader) + i)
         loss.backward()
         optimizer.step()
         y_pred = y_pred.cpu()
+
 
         total_label += y_data.tolist()
         total_pred += y_pred.tolist()
@@ -63,25 +76,39 @@ def sub_train(model_config, nn_model, dataloader, device, writer, epoch, optimiz
     writer.add_scalar('MAPE Score/sub_train', test_mape_score, epoch)
 
 
-def test(model_config, nn_model, dataloader, device, writer, epoch):
+def test(model_config, nn_model, dataloader, device, writer, epoch, criterion):
     with torch.no_grad():
         total_label = []
         total_pred = []
         for i, data in enumerate(dataloader):
             x_data = None
             y_data = None
-            if model_config['model'] == 'DNN':
-                x_data, y_data = data
-            elif model_config['model'] == 'RNN':
-                x_data = data[:][0]
-                y_data = data[:][1]
-            elif model_config['model'] == 'CRNN':
-                x_data = data[:][0]
-                x_data = x_data.transpose(1, 2)
-                y_data = data[:][1]
+            if 'model' in model_config:
+                if model_config['model'] == 'DNN':
+                    x_data, y_data = data
+                elif model_config['model'] == 'RNN':
+                    x_data = data[:][0]
+                    y_data = data[:][1]
+                elif model_config['model'] == 'CRNN':
+                    x_data = data[:][0]
+                    x_data = x_data.transpose(1, 2)
+                    y_data = data[:][1]
+            else:
+                if model_config['model_type'] == 'Custom_DNN':
+                    x_data, y_data = data
+                elif model_config['model_type'] == 'Custom_RNN':
+                    x_data = data[:][0]
+                    y_data = data[:][1]
+                elif model_config['model_type'] == 'Custom_CRNN':
+                    x_data = data[:][0]
+                    x_data = x_data.transpose(1, 2)
+                    y_data = data[:][1]
             if device:
                 x_data = x_data.cuda()
+                y_data = y_data.cuda()
             y_pred = nn_model(x_data).reshape(-1)
+            loss = criterion(y_pred, y_data)
+            writer.add_scalar('Loss/Test MSELoss', loss / 1000, epoch * len(dataloader) + i)
             y_pred = y_pred.cpu()
 
             total_label += y_data.tolist()
@@ -103,6 +130,65 @@ def test(model_config, nn_model, dataloader, device, writer, epoch):
         print("MAE Score : {}".format(test_mae_score))
 
         return {epoch + 1: [test_mae_score, test_mse_score, test_rmse_score, test_r2_score]}
+
+
+def new_train(model_config, count, tb_writer_path, section_message):
+    saver = {}
+    use_cuda = model_config['use_cuda']
+    num_epochs = model_config['epoch']
+    sequence_length = 1
+    if model_config['model_type'] == 'Custom_RNN' or model_config['model_type'] == 'Custom_CRNN':
+        sequence_length = model_config['sequence_length']
+    train_dataloader, \
+    test_dataloader, \
+    valid_dataloader = data_loader.load_path_loss_with_detail_dataset(input_dir=model_config['input_dir'],
+                                                                      model_type=model_config['model_type'],
+                                                                      num_workers=model_config['num_workers'],
+                                                                      batch_size=model_config['batch_size'],
+                                                                      shuffle=model_config['shuffle'],
+                                                                      input_size=sequence_length)
+    nn_model = model.model_load(model_configure=model_config)
+    criterion = optimizer.set_criterion(model_config['criterion'])
+    if use_cuda:
+        criterion = criterion.cuda()
+    optim = optimizer.set_optimizer(model_config['optimizer'], nn_model, model_config['learning_rate'])
+    writer = use_tensorboard.set_tensorboard_writer('{}/{}_{}'.format(tb_writer_path,
+                                                                      section_message,
+                                                                      str(count).zfill(3)))
+    for epoch in range(num_epochs):
+        for i, picked in enumerate(train_dataloader):
+            if model_config['model_type'] == 'Custom_DNN':
+                x_data, y_data = picked
+            elif model_config['model_type'] == 'Custom_RNN':
+                x_data = picked[:][0]
+                y_data = picked[:][1]
+            elif model_config['model_type'] == 'Custom_CRNN':
+                x_data = picked[:][0]
+                x_data = x_data.transpose(1, 2)
+                y_data = picked[:][1]
+            if use_cuda:
+                x_data = x_data.cuda()
+                y_data = y_data.cuda()
+
+            y_pred = nn_model(x_data).reshape(-1)
+            loss = criterion(y_pred, y_data)
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+            writer.add_scalar('Loss/Training MSELoss', loss / 1000, epoch * len(train_dataloader) + i)
+
+        if (epoch + 1) % 10 == 0:
+            sub_train(model_config=model_config, nn_model=nn_model, dataloader=valid_dataloader, device=use_cuda,
+                      writer=writer, epoch=epoch, optimizer=optim, criterion=criterion)
+            output = test(model_config=model_config, nn_model=nn_model, dataloader=test_dataloader, device=use_cuda,
+                          writer=writer, epoch=epoch, criterion=criterion)
+            torch.save({epoch: epoch, 'model': nn_model, 'model_state_dict': nn_model.state_dict()},
+                       "{}/{}_{}_epoch_{}.pt".format(checkpoint_dir, message, str(count).zfill(3), epoch))
+            saver.update(output)
+    use_tensorboard.close_tensorboard_writer(writer)
+
+    saver = pd.DataFrame(saver)
+    saver.to_csv('{}/{}_{}.csv'.format(checkpoint_dir, message, str(count).zfill(3)))
 
 
 def train(model_config, count, writer_path, message, checkpoint_dir, checkpoint=None):
@@ -168,9 +254,9 @@ def train(model_config, count, writer_path, message, checkpoint_dir, checkpoint=
 
 
 if __name__ == '__main__':
-    file_path = 'configurations/v5_v2/'
-    checkpoint_dir = 'checkpoints/v5_v2'
-    writer_path = 'runs_v5_v2'
+    file_path = 'configurations/v5_test/'
+    checkpoint_dir = 'checkpoints/v5_test'
+    writer_path = 'runs_v5_test'
     checkpoint = None # 'checkpoints_all/CRNN_Adam_LeakyReLU_0.001_sl15_010_epoch_729.pt'
     file_list = tool.file_io.get_all_file_path(file_path, file_extension='json')
     for file in file_list:
@@ -180,11 +266,12 @@ if __name__ == '__main__':
         # filename = data_preprocessing.get_pure_filename(file)
         for idx, data in enumerate(json_data):
             print(idx, data)
-            message = "{}_{}_{}_{}_sl{}".format(data['model'], data['optimizer'],
+            message = "{}_{}_{}_{}_sl{}".format(data['model_type'], data['optimizer'],
                                                         data['activation'], data['learning_rate'],
                                                         data['sequence_length'])
             if 'linear' in data:
                 message += "_{}".format(data['linear'])
-            train(model_config=data, count=idx, writer_path=writer_path, message=message, checkpoint_dir=checkpoint_dir, checkpoint=checkpoint)
+            # train(model_config=data, count=idx, writer_path=writer_path, message=message, checkpoint_dir=checkpoint_dir, checkpoint=checkpoint)
+            new_train(model_config=data, count=idx, tb_writer_path=writer_path, section_message=message)
 
 
