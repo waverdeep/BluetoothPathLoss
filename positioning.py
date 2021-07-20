b561dd764088d7ae462d3bcc854746305c2d34e6
waverDeep <dandyleader@naver.com>
2021-07-20 19:43:53 +0900

    패키지로 변경하면서 import 구문 변경

--- positioning.py
+++ positioning.py
@@ -5,70 +5,130 @@ from data import data_loader
 import random
 import time
 import math
-import trilateration_booster_v1.trilateration as trilateration
+from trilateration_booster_v1 import trilateration
 # randomness
-random_seed = 11
-np.random.seed(random_seed)
-random.seed(random_seed)
-torch.manual_seed(random_seed)
-# 연산속도가 느려질 수 있음
-# torch.backends.cudnn.deterministic = True
-# torch.backends.cudnn.benchmark = False
-torch.cuda.manual_seed(random_seed)
-torch.cuda.manual_seed_all(random_seed) # if use multi-GPU
+# random_seed = 11
+# np.random.seed(random_seed)
+# random.seed(random_seed)
+# torch.manual_seed(random_seed)
+# # 연산속도가 느려질 수 있음
+# # torch.backends.cudnn.deterministic = True
+# # torch.backends.cudnn.benchmark = False
+# torch.cuda.manual_seed(random_seed)
+# torch.cuda.manual_seed_all(random_seed) # if use multi-GPU
 
 
-def inference(model_config, file):
-    nn_model = model.model_load(model_config)
+def inference(nn_model, model_config, file):
+    nn_model = nn_model
     inference_dataloader = \
         data_loader.load_path_loss_with_detail_inference_dataset(file, 'CRNN',
                                                                  batch_size=model_config['batch_size'])
     total_pred = []
     with torch.no_grad():
         for i, data in enumerate(inference_dataloader):
-            if model_config['model'] == 'DNN':
-                pass
-            elif model_config['model'] == 'RNN':
-                pass
-            elif model_config['model'] == 'CRNN':
-                x_data = data.transpose(1, 2)
-                x_data = x_data.cuda()
+            if 'model' in model_config:
+                if model_config['model'] == 'DNN':
+                    pass
+                elif model_config['model'] == 'RNN':
+                    pass
+                elif model_config['model'] == 'CRNN':
+                    x_data = data.transpose(1, 2)
+                    x_data = x_data.cuda()
+            elif 'model_type' in model_config:
+                if model_config['model_type'] == 'Custom_DNN':
+                    pass
+                elif model_config['model_type'] == 'Custom_RNN':
+                    pass
+                elif model_config['model_type'] == 'Custom_CRNN':
+                    x_data = data.transpose(1, 2)
+                    x_data = x_data.cuda()
             y_pred = nn_model(x_data).reshape(-1).cpu().numpy()
-            total_pred[len(total_pred):len(y_pred)] = y_pred
+            total_pred.extend(y_pred)
+            # total_pred[len(total_pred):len(y_pred)] = y_pred
 
-            print('mean : ', y_pred.mean())
-            print('median : ', np.median(y_pred))
-            print('max : ', y_pred.max())
-            print('min : ', y_pred.min())
-            return {'mean': y_pred.mean(), 'median': np.median(y_pred),
-                    'max': y_pred.max(), 'min': y_pred.min()}
+        # print('mean : ', y_pred.mean())
+        # print('median : ', np.median(y_pred))
+        # print('max : ', y_pred.max())
+        # print('min : ', y_pred.min())
+        # return {'mean': y_pred.mean(), 'median': np.median(y_pred),
+        #         'max': y_pred.max(), 'min': y_pred.min()}
+        total_pred = np.array(total_pred)
+        print('mean : ', total_pred.mean())
+        print('median : ', np.median(total_pred))
+        print('max : ', total_pred.max())
+        print('min : ', total_pred.min())
+        return {'mean': total_pred.mean(), 'median': np.median(total_pred),
+                'max': total_pred.max(), 'min': total_pred.min()}
 
 
-model_configure = {"model": "CRNN",
-                   "activation": "LeakyReLU",
-                   "cuda": True,
-                   "batch_size": 512,
-                   "input_size": 7,
-                   "sequence_length": 15,
-                   'checkpoint_path': 'checkpoints_all_v3/CRNN_Adam_LeakyReLU_0.001_sl15_000_epoch_769.pt',
+model_configure = {
+    "model_type": "Custom_CRNN", "input_size": 11, "sequence_length": 15, "activation": "ReLU",
+    "convolution_layer": 1, "bidirectional": False, "hidden_size": 256, "num_layers": 1,
+    "linear_layers": [64, 1], "criterion": "MSELoss", "optimizer": "AdamW", "dropout_rate": 0.5,
+    "use_cuda": True, "batch_size": 200, "learning_rate": 0.0001, "epoch": 2000,
+    "num_workers": 8, "shuffle": True, "input_dir": "dataset/v5/newb",
+    "checkpoint_path": "test_checkpoints/quick06-4b_Custom_CRNN_AdamW_ReLU_0.0001_sl15_999_epoch_1279.pt"
 }
 
 if __name__ == '__main__':
     start_time = time.time()
 
-    pol1_data_path = ''
-    pol2_data_path = ''
-    pol3_data_path = ''
+    # pol1_data_path = 'dataset/v5/test3_point_39_0_5/f9-9c-e9-5d-b9-6f_39_0_5_pol1.csv'
+    # pol2_data_path = 'dataset/v5/test3_point_39_0_5/f9-9c-e9-5d-b9-6f_39_0_5_pol2.csv'
+    # pol3_data_path = 'dataset/v5/test3_point_39_0_5/f9-9c-e9-5d-b9-6f_39_0_5_pol3.csv'
+
+    # pol1_data_path = 'dataset/v5/test3_point_39_5_15/04-ee-03-74-ae-a5_39_5_15_pol1.csv'
+    # pol2_data_path = 'dataset/v5/test3_point_39_5_15/04-ee-03-74-ae-a5_39_5_15_pol2.csv'
+    # pol3_data_path = 'dataset/v5/test3_point_39_5_15/04-ee-03-74-ae-a5_39_5_15_pol3.csv'
+
+    # pol1_data_path = 'dataset/v5/test3_point_39_10_5/04-ee-03-74-ae-a5_39_10_5_pol1.csv'
+    # pol2_data_path = 'dataset/v5/test3_point_39_10_5/04-ee-03-74-ae-a5_39_10_5_pol2.csv'
+    # pol3_data_path = 'dataset/v5/test3_point_39_10_5/04-ee-03-74-ae-a5_39_10_5_pol3.csv'
+
+    # pol1_data_path = 'dataset/v5/test3_point_39_10_5/04-ee-03-74-b0-30_39_10_5_pol1.csv'
+    # pol2_data_path = 'dataset/v5/test3_point_39_10_5/04-ee-03-74-b0-30_39_10_5_pol2.csv'
+    # pol3_data_path = 'dataset/v5/test3_point_39_10_5/04-ee-03-74-b0-30_39_10_5_pol3.csv'
+    #
+    # pol1_data_path = 'dataset/v5/test3_point_39_10_10/f9-9c-e9-5d-b9-6f_39_10_10_pol1.csv'
+    # pol2_data_path = 'dataset/v5/test3_point_39_10_10/f9-9c-e9-5d-b9-6f_39_10_10_pol2.csv'
+    # pol3_data_path = 'dataset/v5/test3_point_39_10_10/f9-9c-e9-5d-b9-6f_39_10_10_pol3.csv'
+
+    # pol1_data_path = 'dataset/v5/test3_point_39_10_20/04-ee-03-74-ae-a5_39_10_20_pol1.csv'
+    # pol2_data_path = 'dataset/v5/test3_point_39_10_20/04-ee-03-74-ae-a5_39_10_20_pol2.csv'
+    # pol3_data_path = 'dataset/v5/test3_point_39_10_20/04-ee-03-74-ae-a5_39_10_20_pol3.csv'
+    #
+    # pol1_data_path = 'dataset/v5/test3_point_39_15_10/f9-9c-e9-5d-b9-6f_39_15_10_pol1.csv'
+    # pol2_data_path = 'dataset/v5/test3_point_39_15_10/f9-9c-e9-5d-b9-6f_39_15_10_pol2.csv'
+    # pol3_data_path = 'dataset/v5/test3_point_39_15_10/f9-9c-e9-5d-b9-6f_39_15_10_pol3.csv'
+
+    # pol1_data_path = 'dataset/v5/test3_point_39_15_25/04-ee-03-74-ae-a5_39_15_25_pol1.csv'
+    # pol2_data_path = 'dataset/v5/test3_point_39_15_25/04-ee-03-74-ae-a5_39_15_25_pol2.csv'
+    # pol3_data_path = 'dataset/v5/test3_point_39_15_25/04-ee-03-74-ae-a5_39_15_25_pol3.csv'
+
+    # pol1_data_path = 'dataset/v5/test3_point_39_20_15/04-ee-03-74-ae-a5_39_20_15_pol1.csv'
+    # pol2_data_path = 'dataset/v5/test3_point_39_20_15/04-ee-03-74-ae-a5_39_20_15_pol2.csv'
+    # pol3_data_path = 'dataset/v5/test3_point_39_20_15/04-ee-03-74-ae-a5_39_20_15_pol3.csv'
+    #
+    # pol1_data_path = 'dataset/v5/test3_point_39_20_15/04-ee-03-74-b0-30_39_20_15_pol1.csv'
+    # pol2_data_path = 'dataset/v5/test3_point_39_20_15/04-ee-03-74-b0-30_39_20_15_pol2.csv'
+    # pol3_data_path = 'dataset/v5/test3_point_39_20_15/04-ee-03-74-b0-30_39_20_15_pol3.csv'
+    #
+    pol1_data_path = 'dataset/v5/test_point_00_15/fb-f8-c9-9d-d8-d6_37_0_15_pol1.csv'
+    pol2_data_path = 'dataset/v5/test_point_00_15/fb-f8-c9-9d-d8-d6_37_0_15_pol2.csv'
+    pol3_data_path = 'dataset/v5/test_point_00_15/fb-f8-c9-9d-d8-d6_37_0_15_pol3.csv'
+
+
+    inference_model = model.model_load(model_configure)
 
     circles = [trilateration.Circle(
                     trilateration.Point(0, 0),
-                    inference(model_config=model_configure, file=pol1_data_path)['max']
-                ), trilateration.Circle(
-                    trilateration.Point(30, 0),
-                    inference(model_config=model_configure, file=pol2_data_path)['max']
+                    inference(nn_model=inference_model, model_config=model_configure, file=pol1_data_path)['max']
                 ), trilateration.Circle(
                     trilateration.Point(0, 30),
-                    inference(model_config=model_configure, file=pol3_data_path)['max']
+                    inference(nn_model=inference_model, model_config=model_configure,  file=pol2_data_path)['max']
+                ), trilateration.Circle(
+                    trilateration.Point(30, 0),
+                    inference(nn_model=inference_model, model_config=model_configure,  file=pol3_data_path)['max']
     )]
 
     while True:
