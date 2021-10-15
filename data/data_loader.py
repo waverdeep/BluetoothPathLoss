76e2764da7ed44501e92101573465923b1dd7a09
waverDeep <dandyleader@naver.com>
2021-10-15 16:48:21 +0900

    수정 사항 추가

--- data/data_loader.py
+++ data/data_loader.py
@@ -37,7 +37,7 @@ class PathLossWithDetailDataset(Dataset):
 
 
 def load_path_loss_with_detail_dataset(input_dir, model_type='RNN', num_workers=4, batch_size=128,
-                                       shuffle=True, input_size=10, various_input=True):
+                                       shuffle=True, input_size=15, various_input=False):
     # 파일들이 저장되었는 경로를 받아 파일 리스트를 얻어냄
     file_list = file_io.get_all_file_path(input_dir, file_extension='csv')
     # csv에 있는 모든 데이터를 다 꺼내서 넘파이로 만듬
@@ -64,8 +64,9 @@ def load_path_loss_with_detail_dataset(input_dir, model_type='RNN', num_workers=
         for n_idx, pack in enumerate(div_meter_pack):
             for i in range(len(pack)-input_size):
                 rnn_dataset.append(pack[i:i+input_size])
-            for i in range(len(pack)-input_size):
-                rnn_dataset.append(pack[i:i+np.random.randint(input_size-5)+5])
+            if various_input is True:
+                for i in range(len(pack)-input_size):
+                    rnn_dataset.append(pack[i:i+np.random.randint(input_size-7)+7])
         rnn_dataset = np.array(rnn_dataset)
         setup_dataset = rnn_dataset
 
@@ -76,11 +77,11 @@ def load_path_loss_with_detail_dataset(input_dir, model_type='RNN', num_workers=
                                                       model_type=model_type)
     pathloss_valid_dataset = PathLossWithDetailDataset(input_data=valid_data,
                                                        model_type=model_type)
-    pathloss_train_dataloader = DataLoader(pathloss_train_dataset, batch_size=batch_size, shuffle=shuffle,
+    pathloss_train_dataloader = DataLoader(pathloss_train_dataset, batch_size=batch_size, shuffle=True,
                                            num_workers=num_workers)
-    pathloss_test_dataloader = DataLoader(pathloss_test_dataset, batch_size=batch_size, shuffle=shuffle,
+    pathloss_test_dataloader = DataLoader(pathloss_test_dataset, batch_size=batch_size, shuffle=False,
                                           num_workers=num_workers)
-    pathloss_valid_dataloader = DataLoader(pathloss_valid_dataset, batch_size=batch_size, shuffle=shuffle,
+    pathloss_valid_dataloader = DataLoader(pathloss_valid_dataset, batch_size=batch_size, shuffle=False,
                                            num_workers=num_workers)
     return pathloss_train_dataloader, pathloss_test_dataloader, pathloss_valid_dataloader
 
